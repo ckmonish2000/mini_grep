3330d3f22a6fa3836b0dcfbbf3fd852585cfcee9
monish karunakaran <ckmonish2000@gmail.com>
2022-02-07 21:11:19 +0530

    working iterator

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ fn main() {
 
     if val[0] == "-c"{
         println!("{:#?}",file_contents.as_str().contains(val[1].as_str()));
+    }else if val[0] == "-i"{
+        let iter:Vec<&str> = file_contents.split_whitespace().collect();
+        
+      for i in iter.iter() {
+          println!("word = {} search = {}",i, i == &val[1].as_str());
+      }
     }
 
 }
