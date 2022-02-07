e47b442ee8ad26cdcf8c41cc2edc87e2f39ddcfc
monish karunakaran <ckmonish2000@gmail.com>
2022-02-07 20:48:45 +0530

    working read file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use std::ptr::read;
+
 mod arguments;
+mod readfile;
 fn main() {
     let val = arguments::get_arg();
+
+    readfile::read_file(&val[1]);
     println!("{:#?}",val);
 }
--- src/readfile.rs
+++ src/readfile.rs
@@ -1,7 +1,14 @@
 use std::io::Read;
 use std::fs::File;
 
+// D:\\work\\Playaround\\minigrep \\absolute
+// .\\xy.txt relative
 
-pub fn read_file(path:String){
+
+pub fn read_file(path: &String){
+  let mut file = std::fs::File::open(path).unwrap();
+   let mut contents = String::new();
+   file.read_to_string(&mut contents);
+   print!("{}", contents);
   
 }
